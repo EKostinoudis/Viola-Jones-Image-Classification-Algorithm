43b2f6f1649755eddb21864aa4d12aea6ade7185
EKostinoudis <vagelis789@gmail.com>
2019-03-14 02:44:37 +0200

    Made parallel feature calculation

--- ViolaJonesTrain.py
+++ ViolaJonesTrain.py
@@ -1,13 +1,20 @@
 from utils import readImage, integralImage, Feature
 import numpy as np
 from os import listdir
+from multiprocessing import Pool
+from functools import partial
+
 
 class ViolaJonesTrain:
-    def __init__(self, T, positivePath = "positive", negativePath = "negative"):
+    def __init__(self, T, threads = 4, positivePath = "positive", negativePath = "negative", 
+                    maxPositiveImages = -1, maxNegativeImages = -1):
         """
         T: number of weak classifiers
+        threads: number of threads to run parallel (on some computations)
         poitivePath: folder with the positive images
         negativePath: folder with the negative images
+        maxPositiveImages: mamimum number of positive images (-1: all posible)
+        maxNegativeImages: mamimum number of negative images (-1: all posible)
 
         tainingData: list with the integral image of the training data and the type of the image
                      1 for positve
@@ -16,20 +23,30 @@ class ViolaJonesTrain:
         negativeNumber: number of negative images
         """
         self.T = T
+        self.threads = threads
         
         posImages = listdir(positivePath)
         negImages = listdir(negativePath)
+
+        if maxPositiveImages > -1:
+            posImages = posImages[:maxPositiveImages]
+
+        if maxNegativeImages > -1:
+            negImages = negImages[:maxNegativeImages]
 
         self.positveNumber = len(posImages)
         self.negativeNumber = len(negImages)
 
         self.trainingData = []
+        self.imageClass = []
 
         for im in posImages:
-            self.trainingData.append([integralImage(readImage(positivePath + "/" + im)), 1])
+            self.trainingData.append(integralImage(readImage(positivePath + "/" + im)))
+            self.imageClass.append(1)
 
         for im in negImages:
-            self.trainingData.append([integralImage(readImage(negativePath + "/" + im)), 0])
+            self.trainingData.append(integralImage(readImage(negativePath + "/" + im)))
+            self.imageClass.append(0)
 
     def createFeatures(self):
         """
@@ -38,7 +55,7 @@ class ViolaJonesTrain:
         Returnes:
             a list of features
         """
-        totalHeight, totalWidth = self.trainingData[0][0].shape
+        totalHeight, totalWidth = self.trainingData[0].shape
         features = []
 
         # Type 1 features
@@ -80,19 +97,17 @@ class ViolaJonesTrain:
             2D list with the values of the features
         """
 
-        featureValues = []
-        counter = 0
-        for train in self.trainingData:
-            imageValues = []
-            for feature in features:
-                imageValues.append(feature.calculate(train[0]))
-
-            counter +=1
-            print("Calculated {} out of {}".format(counter, len(self.trainingData)))
-            featureValues.append(imageValues)
+        pool = Pool(self.threads)
+        featureValues = pool.map(partial(self.calculateFeatures, features=features), self.trainingData)
 
         return featureValues
 
+    @staticmethod
+    def calculateFeatures(ii, features):
+        imageValues = []
+        for feature in features:
+            imageValues.append(feature.calculate(ii))
+        return imageValues
 
 
     def trainModel(self):
@@ -102,9 +117,12 @@ class ViolaJonesTrain:
                                   ))
 
         # Calculate all posible features
+        print("Creating features.")
         features = self.createFeatures()
 
         # Calculate for every image the value of every feature
+        print("Calculating features for all images.")
         featureValues = self.applyFeatures(features)
 
+        # return features
         return featureValues
